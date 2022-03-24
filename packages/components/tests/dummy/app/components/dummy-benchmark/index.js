7e04f0a46389360ddbe8c3a91decc7f32e3ce7c8
Cristiano Rastelli <public@didoo.net>
2022-03-24 09:29:31 +0000

    prepared base benchmarking with template-only component

---
+++ packages/components/tests/dummy/app/components/dummy-benchmark/index.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+
+const ELEMENTS = 500;
+const TOGGLES = ['default', 'more', 'user'];
+
+const randomFromArray = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const randomString = () => (Math.random() + 1).toString(36).substring(2);
+const randomBoolean = () => Math.random() > 0.5;
+
+export default class DummyBenchmarkIndexComponent extends Component {
+  get elements() {
+    let elements = [];
+    for (let index = 0; index < ELEMENTS; index++) {
+      let element = {
+        toggle: randomFromArray(TOGGLES),
+        text: randomString(),
+        size: randomBoolean(),
+        color: randomBoolean(),
+        avatarUrl: randomBoolean() ? '/assets/images/avatar.png' : undefined,
+      };
+      elements.push(element);
+    }
+    return elements;
+  }
+}
